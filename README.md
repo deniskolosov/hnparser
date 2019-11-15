c2b92eb9a0c264933581a00ba8a9c9b0ae1d99ff
Denis Kolosov <denis.kolosov@gmail.com>
2019-11-15 15:06:28 +0300

    Add tests and readme

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
+Run locally:
+`
+git clone https://github.com/deniskolosov/hnparser.git
+cd hnparser
+python3 -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+python3 manage.py migrate
+python3 manage.py runserver
+`
+Check running app on localhost:8000
+
+Run in Docker:
+`docker-compose up`
+
+Check running app on localhost:1337
+
+Tests:
+`docker-compose exec web python manage.py test`
 
--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
+from django.urls import reverse
+from freezegun import freeze_time
 
-# Create your tests here.
+from hackernews_parser.tests.factories import PostFactory
+
+
+@freeze_time("2019-01-01")
+class GetPostsTestCase(TestCase):
+    def setUp(self):
+        self.post = PostFactory(title='Foo Bar', url='http://foo.bar')
+        self.post1 = PostFactory(title='Bar Baz', url='http://bar.baz')
+        self.url = reverse('posts-list')
+        self.expected_results = [
+            {'id': 1, 'title': 'Foo Bar', 'url': 'http://foo.bar', 'created_at': '2019-01-01T00:00:00Z'},
+            {'id': 2, 'title': 'Bar Baz', 'url': 'http://bar.baz', 'created_at': '2019-01-01T00:00:00Z'},
+        ]
+
+    def test_get_posts(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+        results = response.json()['results']
+        self.assertEqual(len(results), 2)
+
