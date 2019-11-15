20a6996259aac9c371acaff36a8c121647591456
Denis Kolosov <denis.kolosov@gmail.com>
2019-11-15 15:06:28 +0300

    Add tests and readme

--- api/urls.py
+++ api/urls.py
@@ -2,9 +2,8 @@ from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
 from api import views
-
 router = DefaultRouter()
-router.register(r'posts', views.PostViewSet)
+router.register(r'posts', views.PostViewSet, base_name='posts')
 
 
 urlpatterns = [
